8bc86be4d90fcbdd3128a3898a3c8f11f73c4b04
pinky-banik <pinky.rani.banik@gmail.com>
2022-03-27 07:20:42 +0600

    updated

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { findAllByAltText } from '@testing-library/react';
 
 const UpdateUser = () => {
     const[user,setUser] =useState({});
@@ -13,11 +14,44 @@ const UpdateUser = () => {
         .then(res=>res.json())
         .then(data=>setUser(data))
     },[])
+    const handleUpdateUser=e=>{
+        const url=`http://localhost:5000/users/${id}`;
+        fetch(url,{
+            method:'PUT',
+            headers:{
+               'content-type':'application/json'
+            },
+            body : JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.modifiedCount>0){
+                alert('updated successfully');
+                setUser({});
+            }
+        })
+        e.preventDefault();
+    }
+    const handleNameChange=e=>{
+        const updatedName = e.target.value;
+        const updatedUser={name:updatedName,email:user.email};
+        setUser(updatedUser);
+    }
+    const handleEmailChange=e=>{
+        const updateEmail = e.target.value;
+        const updatedUser={...user};
+        updatedUser.email =updateEmail;
+        setUser(updatedUser);
+    }
     return (
         <div>
             <h2>Update User {user.name}</h2>
             <p>{id}</p>
-
+            <form onSubmit={handleUpdateUser}>
+               <input onChange={handleNameChange} type="text" value={user.name || ''}/>
+               <input onChange={handleEmailChange} type="email" value={user.email || ''}/>
+               <input type="submit" value="update" />
+            </form>
         </div>
     );
 };
